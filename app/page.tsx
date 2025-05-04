cba02ecba0275434e7d75cd53b9aa5123b51d4d0
andreszapataq <andreszapataq@gmail.com>
2025-05-04 13:00:33 -0500

    fix main container styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="min-h-screen py-[90px] px-8 md:px-12 lg:px-[190px]">
+    <div className="min-h-screen p-8 md:py-[90px] md:px-[190px]">
       <Header />
     </div>
   );
